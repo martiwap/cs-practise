6525163148a8db76cfeefe5ba74f48bd7930ad6c
Martina Natale <martinanatale93@gmail.com>
2018-11-25 21:25:24 +0000

    added helloWorld and TypeConversion

---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int serialNo = 100;
+            string accountNo = "";
+            float anyNo = 3.5f;
+
+            accountNo = serialNo.ToString();
+
+            Console.WriteLine(accountNo);
+
+            //serialNo = Convert.ToInt32(anyNo); //serialNo = 4
+            serialNo = (int)anyNo; //serialNo = 3
+
+            Console.WriteLine(serialNo);
+
+        }
+    }
+}
---
+++ helloWorld/helloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
