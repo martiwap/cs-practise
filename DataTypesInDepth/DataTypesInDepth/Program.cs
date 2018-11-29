d310b341050b952625b694eceafa872e024e5e39
Martina Natale <martinanatale93@gmail.com>
2018-11-29 13:51:33 +0000

    DateTime and DataTypes lessons

---
+++ DataTypesInDepth/DataTypesInDepth/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DataTypesInDepth
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = 0;
+
+            x = int.MaxValue; //2147483647
+            Console.WriteLine(x);
+
+            uint y = 2147483648; // int wouldn't contain it, unit would instead
+            Console.WriteLine(y);
+
+            uint min = uint.MinValue; //0
+            Console.WriteLine(min);
+
+            Console.WriteLine(typeof(int).IsPrimitive); //true
+            Console.WriteLine(typeof(decimal).IsPrimitive); //false 
+
+            Console.WriteLine("Let's see the difference between float, double and decimal.");
+            float f_num = 0.0f;
+            double d_num = 0.0d;
+            decimal m_num = 0.0m;
+
+            Console.WriteLine("float : " + f_num + ", double : " + d_num + ", decimal : " + m_num);
+
+            f_num = 1 / 3f;
+            d_num = 1 / 3d;
+            m_num = 1 / 3m;
+
+            Console.WriteLine("It is always the same value, to all three: ");
+            Console.WriteLine("float : " + f_num + ", double : " + d_num + ", decimal : " + m_num);
+        }
+    }
+}
---
+++ DateTimeLecture/DateTimeLecture/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DateTimeLecture
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            DateTime myDate = new DateTime(); //01/01/0001 00:00:00
+            Console.WriteLine(myDate);        // the beginning of time 0.o
+
+            DateTime myBirthday = new DateTime(1993, 11, 10);
+            Console.WriteLine("My birthday is on the " + myBirthday);
+
+            DateTime today = DateTime.Today;
+            Console.WriteLine("Today is " + today);
+
+            DateTime now = DateTime.Now;
+            Console.WriteLine("Literally, now is " + now);
+            Console.WriteLine("This will only display the current date " + now.ToShortDateString());
+            Console.WriteLine("This will only display the current time " + now.ToShortTimeString());
+            Console.WriteLine("This will display the actual date " + now.ToLongDateString());
+
+            string formattedYear = string.Format("Year is {0:yyyy}", now);
+            Console.WriteLine("Formatting the current year in a string: " + formattedYear);
+
+            Console.WriteLine("Let's try this:");
+            DateTime dateToDisplay = new DateTime(2017, 09, 30, 09, 09, 59);
+            string firstFormat = string.Format("{0:dd-MM-yyyy H:mm:ss}", dateToDisplay);
+            string secondFormat = string.Format("{0:dddd} of month {0:MMMM} year {0:yyyy}", dateToDisplay);
+            Console.WriteLine("First format: " + firstFormat);
+            Console.WriteLine("Second format: " + secondFormat);
+            Console.WriteLine("Third format: Day {0:dddd} \n Month {0:MMMM} \n Year {0:yyyy}", dateToDisplay);
+
+        }
+    }
+}
